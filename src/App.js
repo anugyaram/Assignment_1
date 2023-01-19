267b3148ae91aa1f03242396456d8a28ed00873b
Anugya Ram <anugyaram08@gmail.com>
2023-01-19 21:44:33 +0530

    design of header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div>Hello!!</div>
+    <>
+    <Header />
+    </>
   );
 }
 
