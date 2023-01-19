3afe408d5529535f8fc8c5ee841881468e7351c9
Anugya Ram <anugyaram08@gmail.com>
2023-01-19 22:52:54 +0530

    Routing done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,19 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Badge from '@mui/material/Badge';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return(
         <>
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" style={{height:"60px"}}>
             <Container>
-            <Navbar.Brand href="#home">Add to Cart</Navbar.Brand>
+            <NavLink to="/" className="text-decoration-none text-light mx-3">Add to Cart</NavLink>
             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
+                <NavLink to="/cart" className="text-decoration-none text-light">Home</NavLink>
             </Nav>
             
             <Badge badgeContent={4} color="primary">
-                <i class="fa-solid fa-cart-shopping text-light" style={{fontSize:20,cursor:'pointer'}}></i>
+                <i class="fa-solid fa-cart-shopping text-light" style={{fontSize:25,cursor:'pointer'}}></i>
             </Badge>
             </Container>
         </Navbar>
