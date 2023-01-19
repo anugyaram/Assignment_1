d5aa700bfab26612f0fff3f5600edf5d5b32b846
Anugya Ram <anugyaram08@gmail.com>
2023-01-20 00:33:13 +0530

    Home page & menu

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "./style.css";
+
+import CardData from "./CardData";
 
 const Cards = () => {
-  return (
-    <div>Cards</div>
-  )
-}
+  const [data, setData] = useState(CardData);
+  console.log(data);
 
-export default Cards
+  return (
+    <div className="container mt-3">
+      <h2 className="text-center">Add to Cart</h2>
+
+      <div className="row d-flex align-items-center justify-content-center">
+        {data.map((element, id) => {
+          return (
+            <>
+              <Card
+                style={{ width: "22rem", border: "none" }}
+                className="mx-2 mt-4 card_style"
+              >
+                <CardMedia
+                  variant="top"
+                  className="mt-3"
+                  style={{ height: "16rem" }}
+                  image={element.imgdata}
+                  title={element.rname}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {element.rname}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Price: ₹ {element.price}
+                  </Typography>
+                  <div className="button_div d-flex justify-content-center">
+                    <Button variant="contained" className="col-lg-12 mt-2">Add to Cart</Button>
+                  </div>
+                </CardContent>   
+              </Card>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,69 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Badge from '@mui/material/Badge';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Badge from "@mui/material/Badge";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return(
-        <>
-        <Navbar bg="dark" variant="dark" style={{height:"60px"}}>
-            <Container>
-            <NavLink to="/" className="text-decoration-none text-light mx-3">Add to Cart</NavLink>
-            <Nav className="me-auto">
-                <NavLink to="/cart" className="text-decoration-none text-light">Home</NavLink>
-            </Nav>
-            
-            <Badge badgeContent={4} color="primary">
-                <i class="fa-solid fa-cart-shopping text-light" style={{fontSize:25,cursor:'pointer'}}></i>
-            </Badge>
-            </Container>
-        </Navbar>
-        </>
-    );
-}
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" style={{ height: "60px" }}>
+        <Container>
+          <NavLink to="/" className="text-decoration-none text-light mx-3">
+            Add to Cart
+          </NavLink>
+          <Nav className="me-auto">
+            <NavLink to="/cart" className="text-decoration-none text-light">
+              Home
+            </NavLink>
+          </Nav>
+
+          <Badge
+            badgeContent={4}
+            color="primary"
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <i
+              class="fa-solid fa-cart-shopping text-light"
+              style={{ fontSize: 25, cursor: "pointer" }}
+            ></i>
+          </Badge>
+        </Container>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+        <div className="card-details d-flex justify-content-center align-items-center" style={{width:"24rem",padding:10,position:"relative"}}>
+            <i className="fas fa-close smallclose" style={{position:"absolute",top:2,right:20,fontSize:23,cursor:"pointer"}}
+            onClick={handleClose}></i>
+            <p>Your cart is empty</p>
+            <img src="./cart.gif" alt="" className="emptycart_img" style={{width:"5rem",padding:10}}/>
+        </div>
+        </Menu>
+      </Navbar>
+    </>
+  );
+};
 
 export default Header;
